9b28d9f432f61f99a157479c6dc2431de3f1a39b
bernylinville <bernylinville@devopsthink.org>
2021-04-23 15:40:30 +0800

    update

--- c/headfirstc/cards.c
+++ c/headfirstc/cards.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-    char card_name[3]; // see cards_strings.md
+    char card_name[3];
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
@@ -33,6 +33,11 @@ int main()
     {
         val = atoi(card_name);
     }
-    printf("The card value is: %i\n", val);
+    /* Check if the value is 3 to 6 */
+    if ((val > 2) && (val < 7))
+        puts("Count has gone up");
+    /* Otherwise check if the card was 10, J, Q, or K */
+    if (val == 10)
+        puts("Count has gone down");
     return 0;
 }
