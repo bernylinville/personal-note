3de0a1541a074e96a9a98f42e8709cc15e92c2cf
bernylinville <bernylinville@devopsthink.org>
2021-04-23 12:35:56 +0800

    fix

--- c/headfirstc/cards.c
+++ c/headfirstc/cards.c
@@ -9,7 +9,7 @@
 
 int main()
 {
-    char card_name[3]; \\ see cards.md
+    char card_name[3]; // see cards_strings.md
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
