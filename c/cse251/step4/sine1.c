f4009dd0bdacdf4204b08a87acfdf08c715ccb0a
Berny Linville <bernylinville@devopsthink.org>
2021-05-11 14:08:50 +0800

    Update sine1.c

--- c/cse251/step4/sine1.c
+++ c/cse251/step4/sine1.c
@@ -9,17 +9,21 @@
 int main()
 {
     double angle;
-    int numSteps = 20;
+    int numSteps;
     double maxAngle = M_PI * 2;
     double sinVal;
     int i;
     int j;
-    float numSpaces;
+    double numSpaces;
+    printf("Input the number of steps: ");
+	scanf("%d",&numSteps);
 
     for(i = 0; i <= numSteps;i++) {
         angle = (double)i / numSteps * maxAngle;
         sinVal = sin(angle);
         numSpaces = 30 + sinVal * 30;
+        int a = (int)(numSpaces * 100);
+        numSpaces = a / 100.0;
         printf("%3d: %5.2f ", i, angle);
         for (j = 0; j < numSpaces; j++) {
             printf(" ");
