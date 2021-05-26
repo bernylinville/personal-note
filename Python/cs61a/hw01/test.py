62858518935562a7e7e6aed0ce1b385a61e74f5f
Kyle Chou <kylechou@devopsthink.org>
2021-05-26 15:36:52 +0800

    add cs61a

---
+++ Python/cs61a/hw01/test.py
@@ -0,0 +1,7 @@
+from doctest import run_docstring_examples
+
+import sys
+# sys.path.append('/Users/kchou/Downloads/hw01')
+from hw01 import *
+
+run_docstring_examples(two_of_three, globals(), True)
